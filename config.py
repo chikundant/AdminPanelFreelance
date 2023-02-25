2ed4ef3d577f276f8602459ce1047c26e46a0d99
Chikunda <chikunda.dima@gmail.com>
2023-02-25 21:45:29 +0200

    Login registration

---
+++ config.py
@@ -0,0 +1,10 @@
+import os
+
+KEY = 'you-will-never-guess'
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+
+class Config(object):
+    SECRET_KEY = os.environ.get('SECRET_KEY') or KEY
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'main.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
